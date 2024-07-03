a3736244a406eef480bd3dc9b5d7926f7e2681a7
Vivian06-glitch <akoanivivian@gmail.com>
2024-07-03 22:51:07 +0100

    My Journal API calling

--- lib/HomePage/home_page.dart
+++ lib/HomePage/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:news_app/service/services.dart';
+
 import 'package:news_app/Widget/widget.dart';
 
 import '../Model/model.dart';
